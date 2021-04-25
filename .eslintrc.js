aaa96f61720e7510d70d89f20e825f4692baea5e
SevenCats <sevencats@tripsit.me>
2021-04-25 08:02:29 -0400

    get help command working

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,4 +16,25 @@ module.exports = {
   rules: {
     'prefer-destructuring': 0,
   },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.test.ts',
+        '**/__mocks__/*.ts',
+      ],
+      extends: [
+        'airbnb-typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:jest/all',
+      ],
+      env: {
+        'jest/globals': true,
+      },
+      rules: {
+        'jest/require-top-level-describe': 0,
+        'jest/no-hooks': 0,
+      },
+    },
+  ],
 };
--- src/middleware/command.ts
+++ src/middleware/command.ts
@@ -6,21 +6,17 @@ import { BaseDI } from '../types';
 export default function commandMiddleware(
   { logger }: BaseDI,
 ): (message: Message) => void {
-  return ({ channel, content }) => {
+  return async ({ channel, content }) => {
     // Only recognize messages starting with the command prefix
     if (content.trim().charAt(0) === COMMAND_PREFIX) {
       // Break message up into arguments
       const [commandName, ...args] = content.trim().slice(1).split(/\s+/g);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const command: Command = commands[commandName];
-      if (!command) {
-        channel.send('Command not found.').catch((ex) => {
-          logger.error(ex);
-        });
-      } else {
-        command({ channel, logger }, ...args).catch((error) => {
-          logger.error(error);
-        });
+      try {
+        if (!command) await channel.send('Command not found.');
+        else await command({ channel, logger }, ...args);
+      } catch (ex) {
+        logger.error(ex);
       }
     }
   };
